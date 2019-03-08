269ec11b14e40b4c6adf57bbe58f48300475721b
1imartinez <42749604+1imartinez@users.noreply.github.com>
2019-03-08 13:57:21 -0600

    Add files via upload

--- toggleFunctions.js
+++ toggleFunctions.js
@@ -1,31 +1,37 @@
 function green25(){
     const doc = document;
-    let body = doc.querySelector(green25())
-    
+    let body = doc.querySelector("body");
+    body.classList.add("green25")
 
 }
 
 
 function green50(){
-    
+    const doc = document;
+    let body = doc.querySelector("body");
+    body.classList.add("green50")
 
 }
 
 
 function green75(){
-
+    const doc = document;
+    let body = doc.querySelector("body");
+    body.classList.add("green75")
 
 }
 
 
 function green100(){
-
+    const doc = document;
+    let body = doc.querySelector("body");
+    body.classList.add("green100")
 
 }
 
 
 function smallToggle(){
-
+    
 
 }
 
